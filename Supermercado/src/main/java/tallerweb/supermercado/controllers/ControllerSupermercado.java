00c3967b06eb4c568d8847ab02419adfc756215a
julianarabunal <julirabu@gmail.com>
2015-07-06 22:16:00 -0300

    agregarProductosEnStock
    creamos 4 productos y los agregamos al stock

--- Supermercado/src/main/java/tallerweb/supermercado/controllers/ControllerSupermercado.java
+++ Supermercado/src/main/java/tallerweb/supermercado/controllers/ControllerSupermercado.java
@@ -1,7 +1,6 @@
 package tallerweb.supermercado.controllers;
 
 import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,16 +15,39 @@ public class ControllerSupermercado {
 //		INDEX (vista: index.jsp)
 	@RequestMapping(value="/index", method = RequestMethod.GET)
 	public  ModelAndView index(){
+		
 		Stock productosEnStock = Stock.getInstance();
-		productosEnStock.agregarProducto(new Producto("pan",30.0)3));
-		ModelAndView modelAndView = new ModelAndView("index");
 		
-	
+		// CARGA DE PRODUCTOS 
 		
+		Producto pan=new Producto();
+		pan.setNombre("Pan Lactal");
+		pan.setPrecio(30.0);
+		Producto arroz=new Producto();
+		arroz.setNombre("Arroz Gallo");
+		arroz.setPrecio(15.0);
+		Producto gaseosa=new Producto();
+		gaseosa.setNombre("Coca Cola");
+		gaseosa.setPrecio(22.0);
+		Producto fideos=new Producto();
+		fideos.setNombre("Mostacholes");
+		fideos.setPrecio(19.5);
 		
+		productosEnStock.agregarProducto(pan);
+		productosEnStock.agregarProducto(arroz);
+		productosEnStock.agregarProducto(gaseosa);
+		productosEnStock.agregarProducto(fideos);
 		
+		productosEnStock.agregarStock(pan, 10);
+		productosEnStock.agregarStock(arroz, 20);
+		productosEnStock.agregarStock(gaseosa, 50);
+		productosEnStock.agregarStock(fideos, 30);
 		
-		return modelAndView;		
+		ModelAndView agregarStock = new ModelAndView("index");
+		agregarStock.addObject(agregarStock); //NOSE SI ESTO ES ASI, PERO AL MENOS NO TIRA ERROR. ES UNA DE LAS SUGERENCIAS QUE ME OFRECE AL PONER "."
+		agregarStock.setViewName("listaProductosEnStock");
+		return agregarStock;
+				
 	}
 	
 	
@@ -33,11 +55,11 @@ public class ControllerSupermercado {
 //		VER STOCK (vista: stock.jsp)
 	
 	
-	@RequestMapping(value="/stock", method = RequestMethod.GET)
-	public  ModelAndView stock(){
-		ModelAndView modelAndView = new ModelAndView("stock");
-		return modelAndView;		
-	}
+//	@RequestMapping(value="/stock", method = RequestMethod.GET)
+//	public  ModelAndView stock(){
+//		ModelAndView modelAndView = new ModelAndView("stock");
+//		return modelAndView;		
+//	}
 	
 //	ALTA PRODUCTO NUEVO (vista: alta.jsp)
 
